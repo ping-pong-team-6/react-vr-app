fc6037781c51b45c47755f1e493f625e80b83fb2
Jaz Singh <jsingh@grailbio.com>
2017-08-24 20:32:48 -0700

    Convert to a button, make URLs work

--- src/links/vr-links.vr.js
+++ src/links/vr-links.vr.js
@@ -5,6 +5,8 @@ import {
   Pano,
   Text,
   View,
+  VrButton,
+  NativeModules,
 } from 'react-vr';
 import * as api from './api';
 
@@ -24,6 +26,10 @@ export class VrLinks extends React.Component {
       this.setState({links})
     })
   }
+  onClick = (url) => {
+    console.log(url);
+    NativeModules.LinkingManager.openURL(url);
+  }
   render = () => {
     const {links} = this.state;
     const {color = 'white', backgroundColor = 'black'} = this.props;
@@ -31,22 +37,27 @@ export class VrLinks extends React.Component {
     return (
       <View>
         {links.map( ({name, url}, index) => (
-          <Text
+          <VrButton
             key={index}
-            style={{
-              backgroundColor,
-              color,
-              fontSize: 0.8,
-              fontWeight: '400',
-              layoutOrigin: [0.5, 0.5],
-              paddingLeft: 0.2,
-              paddingRight: 0.2,
-              textAlign: 'center',
-              textAlignVertical: 'center',
-              transform: [{translate: [0, 0, -3]}, {rotateX:-30}],
-            }}>
-            {name}
-          </Text>
+            style={{width: 1.5}}
+            onClick={this.onClick.bind(this, url)}
+          >
+            <Text
+              style={{
+                backgroundColor,
+                color,
+                fontSize: 0.8,
+                fontWeight: '400',
+                layoutOrigin: [0.5, 0.5],
+                paddingLeft: 0.2,
+                paddingRight: 0.2,
+                textAlign: 'center',
+                textAlignVertical: 'center',
+                transform: [{translate: [0, 0, -3]}, {rotateX:-30}],
+              }}>
+              {name}
+            </Text>
+          </VrButton>
         ))}
       </View>
     );
