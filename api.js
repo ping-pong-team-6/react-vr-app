6abddc4943ebc5a14dabbb058d325b6af083804a
Jaz Singh <jsingh@grailbio.com>
2017-08-24 20:50:35 -0700

    Add post and delete api calls

--- api.js
+++ api.js
@@ -1,5 +1,21 @@
 export const ENDPOINT = 'http://localhost:42666/api/v1/go-links';
 
 export const getLinks = () => {
-	return fetch(ENDPOINT).then(res => res.json());
+  return fetch(ENDPOINT).then(res => res.json());
+}
+
+export const postGoLink = ({name, url}) => {
+  return fetch(ENDPOINT, {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({name, url}),
+  })
+    .then(response => response.json());
+}
+
+export const deleteGoLink = ({name}) => {
+  return fetch(`${ENDPOINT}/${name}`, {
+    method: 'DELETE',
+  })
+    .then(response => response.json());
 }
